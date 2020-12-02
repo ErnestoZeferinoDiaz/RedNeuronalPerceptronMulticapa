d4fb55ddfd04f53cb438009cf8461c02296f7a9f
Ernesto Zeferino Diaz <ernesto.zefe.d@gmail.com>
2020-12-01 21:13:10 -0600

    Sacando mas datos en el Preprocessing

--- libs/functions.py
+++ libs/functions.py
@@ -9,22 +9,26 @@ def preprocessingIMG(img):
     gray    = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)  
     border  = cv2.Canny(gray,50,200)  
     resp    = np.matrix(border)
-    return resp.getA1()
+    return resp
 
 def get_square(image,square_size):
-  height,width,alt=image.shape
+  alt    = len(image.shape)
+  if(alt==2):
+    image = image[..., np.newaxis]
+  height = image.shape[0]
+  width  = image.shape[1]
+  dime   = image.shape[2]
+  
   if(height>width):
     differ=height
   else:
     differ=width
 
-  mask = np.ones((differ,differ,3), dtype="uint8")*255
+  mask = np.ones((differ,differ,dime), dtype="uint8")*255
   x_pos=int((differ-width)/2)
   y_pos=int((differ-height)/2)
   mask[y_pos:y_pos+height,x_pos:x_pos+width]=image[0:height,0:width]
   mask=cv2.resize(mask,(square_size,square_size),interpolation=cv2.INTER_AREA)
-
-  return mask
 
 
 def ls(ruta = '.'):
--- preprocessing.py
+++ preprocessing.py
@@ -11,11 +11,21 @@ for idx_path,path in enumerate(directory):
     print(path)    
     for path_img in ls(path):
         img  = cv2.imread(path_img)
-        resp = preprocessingIMG(img)
-        X.append(resp)
+        resp1 = preprocessingIMG(img)
+        resp2 = cv2.rotate(resp1, cv2.ROTATE_90_CLOCKWISE)
+        resp3 = cv2.rotate(resp2, cv2.ROTATE_90_CLOCKWISE)
+        resp4 = cv2.rotate(resp3, cv2.ROTATE_90_CLOCKWISE)
+
+        X.append(resp1.getA1())
+        X.append(resp2.getA1())
+        X.append(resp3.getA1())
+        X.append(resp4.getA1())
         
         tmp = np.zeros(len(directory))
         tmp[idx_path]=1
+        Y.append(tmp)
+        Y.append(tmp)
+        Y.append(tmp)
         Y.append(tmp)
 
 X = np.matrix(X)
